e07f21a9412e7032a1c07c9cd1479e2792654775
Douglas Lassance <douglassance@gmail.com>
2020-10-14 21:38:37 -0700

    Change baked version

--- airstorm/__init__.py
+++ airstorm/__init__.py
@@ -5,7 +5,7 @@ Base class.
 __author__ = "Douglas Lassance"
 __license__ = "MIT"
 __copyright__ = "2020, Douglas Lassance"
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 __email__ = "douglassance@gmail.com"
 
 # TODO: Field validation.
